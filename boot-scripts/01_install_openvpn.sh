1328380718fabcac8047934398e4cd535ef355e2
Paul Allen <paul.allen@lithium.com>
2014-10-05 13:15:33 -0500

    fixing the open vpn install

--- boot-scripts/01_install_openvpn.sh
+++ boot-scripts/01_install_openvpn.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+openvpn_version=2.3.4
+
+yum -y groupinstall "Development Tools"
+yum -y install openssl-devel lzo-devel pam-devel
+
 tmp_dir="$(mktemp -d)"
 (
     cd "$tmp_dir"
-    RC=0
-    while $RC -ne 0; do
-	wget http://swupdate.openvpn.org/community/releases/openvpn-2.3.4.tar.gz
-	RC=!?
-	if $RC -ne 0; then
+    RC="1"
+    while test "$RC" != "0"; do
+	wget "http://swupdate.openvpn.org/community/releases/openvpn-${openvpn_version}.tar.gz"
+	RC="$?"
+	if test "$RC" != "0"; then
 	    sleep 1
 	fi
     done
-
+    tar xfz "openvpn-${openvpn_version}.tar.gz"
+    (
+	cd openvpn-${openvpn_version}
+	./configure
+	make
+	make install
+    )
 )
 rm -rf "$tmp_dir"
