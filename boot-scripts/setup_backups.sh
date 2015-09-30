749a0e4dea8af176bc6972424517b42f7531d4f2
Paul Allen <pauldallen@gmail.com>
2015-09-30 14:47:09 +0200

    default cron and bucket region

--- boot-scripts/setup_backups.sh
+++ boot-scripts/setup_backups.sh
@@ -19,6 +19,7 @@ fi
 MY_AZ="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone)"
 MY_REGION="$(echo ${MY_AZ%?})"
 backup_cron="${VPN_BACKUP_CRON:-0 * * * *}"
+backup_bucket_region="${BACKUP_BUCKET_REGION:-us-east-1}"
 
 ## lets set up pre and post restore scripts
 script_dir="/var/tmp/cloudcoreo-directory-backup-scripts"
@@ -38,8 +39,8 @@ EOF
 ## now we need to perform the restore
 (
     cd /opt/; 
-    python cloudcoreo-directory-backup.py --s3-backup-region ${BACKUP_BUCKET_REGION} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/vpn/${ENV}/${VPN_NAME} --directory /etc/passwd --directory /etc/group --directory /etc/shadow --dump-dir /tmp --restore --post-restore-script "${script_dir}/post-restore.sh" --pre-restore-script "${script_dir}/pre-restore.sh"
+    python cloudcoreo-directory-backup.py --s3-backup-region ${backup_bucket_region} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/vpn/${ENV}/${VPN_NAME} --directory /etc/passwd --directory /etc/group --directory /etc/shadow --dump-dir /tmp --restore --post-restore-script "${script_dir}/post-restore.sh" --pre-restore-script "${script_dir}/pre-restore.sh"
 )
 
 ## now that we are restored, lets set up the backups
-echo "${VPN_BACKUP_CRON} ps -fwwC python | grep -q cloudcoreo-directory-backup || { cd /opt/; nohup python cloudcoreo-directory-backup.py --s3-backup-region ${BACKUP_BUCKET_REGION} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/vpn/${ENV}/${VPN_NAME} --directory /etc/passwd --directory /etc/group --directory /etc/shadow --dump-dir /tmp & }" | crontab
+echo "${backup_cron} ps -fwwC python | grep -q cloudcoreo-directory-backup || { cd /opt/; nohup python cloudcoreo-directory-backup.py --s3-backup-region ${backup_bucket_region} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/vpn/${ENV}/${VPN_NAME} --directory /etc/passwd --directory /etc/group --directory /etc/shadow --dump-dir /tmp & }" | crontab
