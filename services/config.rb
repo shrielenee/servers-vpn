e4329e8ac71cfa1ac7103af6b1757c175f2c9710
Paul Allen <pauldallen@gmail.com>
2015-10-27 22:28:02 -0500

    need to add the find on the public subnets too

--- services/config.rb
+++ services/config.rb
@@ -26,6 +26,17 @@ coreo_aws_vpc_subnet "${PRIVATE_SUBNET_NAME}" do
   vpc "${VPC_NAME}"
 end
 
+coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}" do
+  action :find
+  vpc "${VPC_NAME}"
+end
+
+coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}" do
+  action :find
+  route_table "${PUBLIC_ROUTE_NAME}"
+  vpc "${VPC_NAME}"
+end
+
 coreo_aws_s3_policy "${BACKUP_BUCKET}-policy" do
   action :sustain
   policy_document <<-EOF
