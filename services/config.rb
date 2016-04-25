5adcdeab5082282e39b1be34d1cbaee3ae2fcb26
Paul Allen <pauldallen@gmail.com>
2016-04-25 11:15:12 -0700

    finds should call out unique names

--- services/config.rb
+++ services/config.rb
@@ -10,28 +10,28 @@
 ##   internet_gateway true
 ## end
 ##
-coreo_aws_vpc_vpc "${VPC_NAME}" do
+coreo_aws_vpc_vpc "${VPC_NAME}-vpn" do
   action :find
   cidr "${VPC_OCTETS}/16"
 end
 
-coreo_aws_vpc_routetable "${PRIVATE_ROUTE_NAME}" do
+coreo_aws_vpc_routetable "${PRIVATE_ROUTE_NAME}-vpn" do
   action :find
   vpc "${VPC_NAME}"
 end
 
-coreo_aws_vpc_subnet "${PRIVATE_SUBNET_NAME}" do
+coreo_aws_vpc_subnet "${PRIVATE_SUBNET_NAME}-vpn" do
   action :find
   route_table "${PRIVATE_ROUTE_NAME}"
   vpc "${VPC_NAME}"
 end
 
-coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}" do
+coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}-vpn" do
   action :find
   vpc "${VPC_NAME}"
 end
 
-coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}" do
+coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}-vpn" do
   action :find
   route_table "${PUBLIC_ROUTE_NAME}"
   vpc "${VPC_NAME}"
