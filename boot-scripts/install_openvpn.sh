a9add2642e289dccdd2348de42a09506f1aca3e2
root <root@cloudcoreo-vpn.21v0vg0ur5le3cn1a3ev41sttg.dx.internal.cloudapp.net>
2016-12-06 06:12:43 +0000

    apt-get on ubuntu

--- boot-scripts/install_openvpn.sh
+++ boot-scripts/install_openvpn.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-yum --enablerepo=* install -y openvpn easy-rsa zip 
+if python -mplatform | grep -qi Ubuntu; then
+    apt-get install -y openvpn easy-rsa zip
+else
+    yum --enablerepo=* install -y openvpn easy-rsa zip
+fi
